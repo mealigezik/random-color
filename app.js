457b021ba2db9d3ed6a5429494d96b588e99af2f
Ali Gezik <bilgepanter@gmail.com>
2023-02-06 23:49:01 +0100

    Finished

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ btn.addEventListener(`click`, (e) =>{
     //The number 16,777,215 is the total possible combinations of RGB(255,255,255)
     const x =`#` + Math.floor(Math.random()*16777215).toString(16);
     b.style.backgroundColor = x;
-    text.innerHTML = `Your random color is ${x}`;
+    text.innerHTML = `Random color is ${x}`;
 })
